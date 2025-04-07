3636d65ecbee038a5a8c6d7ecd2e90f33416c8fa
AdiBaheti <agbmarvel123@gmail.com>
2025-04-08 01:45:17 +0530

    Created Contact Page

---
+++ contact.php
@@ -0,0 +1,20 @@
+<?php
+include 'navbar.php'
+?>
+
+<style>
+.empty__page {
+  height: 30vh;
+  text-align: center;
+  display: grid;
+  place-content: center;
+}
+</style>
+
+<section class="empty__page">
+  <h1>Contact Page</h1>
+</section>
+
+<?php
+include 'footer.php';
+?>
